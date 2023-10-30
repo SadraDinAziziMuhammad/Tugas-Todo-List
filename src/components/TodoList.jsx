b7014d427ca41ca2e6cd60cbbe931524517cbb8d
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-10-30 20:44:58 +0700

    Fix for Deploy

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTodo, addTodo } from "../redux/reducers/todo-reducer";
 
